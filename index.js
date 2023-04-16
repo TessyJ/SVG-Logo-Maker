e74ea9291c1d394c5875a7373744541346fffa4e
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-04-16 18:21:43 -0400

    Created prompt questions for user to create logo

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hello');
+const readline = require('readline')
+
+const read = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
+//prompt user to enter text for logo
+read.question("Enter Text for logo (Up to 3 characters): ", (text) =>{
+
+    // prompt usesr to enter text color for logo
+    read.question("Enter text color (text or hexadecimal): ", (textcolor) =>{
+
+        // prompt user to select shape for logo
+        read.question("Choose shape for logo (Circle, Square Or Triangle): " , (shape) =>{
+
+            // prompt user to choose logo color
+            read.question("Enter shape color (text or hexadecimal):",(shape) => {
+
+                //function to generate logo
+            })
+        })
+    })
+})
